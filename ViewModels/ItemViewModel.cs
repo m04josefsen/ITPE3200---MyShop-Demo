479e807ef2d920abde42807050d0dc3ba4cb35a5
Marco Josefsen <majos6959@oslomet.no>
2025-09-07 12:42:11 +0200

    MyShop demo - Week 1 to 3

---
+++ ViewModels/ItemViewModel.cs
@@ -0,0 +1,16 @@
+using MyShop.Models;
+
+namespace MyShop.ViewModels
+{
+    public class ItemsViewModel
+    {
+        public IEnumerable<Item> Items;
+        public string? CurrentViewName;
+
+        public ItemsViewModel(IEnumerable<Item> items, string? currentViewName)
+        {
+            Items = items;
+            CurrentViewName = currentViewName;
+        }
+    }
+}
